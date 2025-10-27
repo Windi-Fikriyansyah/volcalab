de8b1836eb23be1125be3f0939cfe2bab873fde2
windi fikriyansyah <diwin321@gmail.com>
2025-10-27 09:12:30 +0700

    cxcx

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-def index(request):
-    return render(request, 'main/index.html')
+# def index(request):
+#     return render(request, 'main/index.html')
 
-def funfact(request):
-    return render(request, 'main/funfact.html')
-def wahkokbisa(request):
-    return render(request, 'main/wahkokbisa.html')
-def howtouse(request):
-    return render(request, 'main/howtouse.html')
-def product(request):
-    return render(request, 'main/product.html')
+# def funfact(request):
+#     return render(request, 'main/funfact.html')
+# def wahkokbisa(request):
+#     return render(request, 'main/wahkokbisa.html')
+# def howtouse(request):
+#     return render(request, 'main/howtouse.html')
+# def product(request):
+#     return render(request, 'main/product.html')
